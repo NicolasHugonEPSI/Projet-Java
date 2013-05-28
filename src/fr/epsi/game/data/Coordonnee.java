b2b3dbc256ad4a76b65a27b0227050d1877a7fac
NicolasHugonEPSI <hugon.nicolas19@gmail.com>
2013-05-28 14:47:06 +0200

    Code : Coordonnées
    Implémentation des méthodes et création de la classe

---
+++ src/fr/epsi/game/data/Coordonnee.java
@@ -0,0 +1,67 @@
+package fr.epsi.game.data;
+
+public class Coordonnee {
+	
+		private int ligne;
+		private int colonne;
+		
+		/**
+		 * Constructeur pour une coordonnee a partir de sa representation text (A5, C6)
+		 * @param coord La coordonnee
+		 */
+		public Coordonnee(String coord)
+		{
+			char valueColonne = coord.charAt(0);
+			
+			
+			
+			
+			String valueLigne = coord.substring(1);
+			this.ligne = Integer.parseInt(valueLigne) - 1;
+		}
+
+		/**
+		 * Constructeur pour une coordonnee
+		 * @param ligne le numero de la ligne
+		 * @param colonne le numero de la colonne
+		 */
+		public Coordonnee(int ligne, int colonne)
+		{
+
+			this.ligne = ligne;
+			this.colonne = colonne;
+			
+		}
+			
+		/**
+		 * Retourne l'indice de la ligne
+		 * @return the ligne
+		 */
+		public int getLigne()
+		{
+			return this.ligne;
+		}
+
+		/**
+		 * Retourne l'indice de la colonne
+		 * @return the colonne
+		 */
+		public int getColonne()
+		{
+			
+			return this.colonne;
+			
+		}
+
+			
+		/**
+		 * Redefini le toString pour afficher une coordonnee sous la forme A0, B5, D1
+		 */
+		public String toString() {
+			
+			
+			return "Return";
+		}
+
+
+}
